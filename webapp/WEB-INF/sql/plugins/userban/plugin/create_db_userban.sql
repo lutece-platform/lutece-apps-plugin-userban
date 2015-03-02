335682841c416b711fe129452144cbfec7009ae3
ryahiaoui <ryahiaoui@users.noreply.github.com>
2015-03-02 14:36:51 +0000

    ADVERT-73:  migration Bootstrap v.3
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/applications/plugin-userban@73950 bab10101-e421-0410-a517-8ce0973de3ef

--- webapp/WEB-INF/sql/plugins/userban/plugin/create_db_userban.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS userban_user;
-CREATE TABLE IF NOT EXISTS userban_user (	
-		id varchar(255),
-		state varchar(255),
-		date timestamp,
-		commentaire varchar(255),
-		motif varchar(255),
-	PRIMARY KEY  (guid)
-) COMMENT='Contient les banissement d''utilisateurs';
